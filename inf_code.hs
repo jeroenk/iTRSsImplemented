4af28a820782cc6bc6151067bfa0ec5d5f92a63d
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2010-06-12 12:28:51 +0200

    Introduce substitutions

--- inf_code.hs
+++ inf_code.hs
@@ -1,13 +1,15 @@
-class Signature a where
-    arity :: a -> Int
+class Signature s where
+    arity :: s -> Int
 
-data Signature a => Infinite_Terms a b =
-    Function a [Infinite_Terms a b]
-    | Variable b
+class Eq v => Variables v
 
-constant :: Signature a => a -> Infinite_Terms a b
-constant x = if arity(x) == 0
-             then Function x []
+data (Signature s, Variables v) => Infinite_Terms s v =
+    Function s [Infinite_Terms s v]
+    | Variable v
+
+constant :: (Signature s, Variables v) => s -> Infinite_Terms s v
+constant c = if arity(c) == 0
+             then Function c []
              else error "Input is not a constant"
 
 class MyShow a where
@@ -16,34 +18,78 @@ class MyShow a where
 instance MyShow Char where
     myshow x = [x]
 
-instance (MyShow a, MyShow b, Signature a) => Show (Infinite_Terms a b) where
-    show (Function x []) = myshow x
-    show (Function x y)  = myshow x ++ "(" ++ (show' y True) ++ ")"
+instance (MyShow s, MyShow v, Signature s, Variables v)
+         => Show (Infinite_Terms s v) where
+    show (Function f []) = myshow f
+    show (Function f xs)  = myshow f ++ "(" ++ (show' xs True) ++ ")"
         where show' [] _         = ""
               show' (x:xs) True  = show x ++ show' xs False
               show' (x:xs) False = "," ++ show x ++ show' xs False
-    show (Variable x)   = myshow x
+    show (Variable v)   = myshow v
 
 data Nat_Strings = Nat_String [Int]
 
 instance Show Nat_Strings where
-    show (Nat_String x) = show x
+    show (Nat_String ns) = show ns
 
-prefix_positions :: [[Nat_Strings]] -> [Nat_Strings]
-prefix_positions xs = concat (prefix_positions' xs 1)
-    where prefix_position n (Nat_String xs) = Nat_String (n:xs)
-          prefix_positions' [] _
+prefix_position :: Int -> Nat_Strings -> Nat_Strings
+prefix_position n (Nat_String ns) = Nat_String (n:ns)
+
+positions :: (Signature s, Variables v) => (Infinite_Terms s v) -> [Nat_Strings]
+positions (Function _ xs)
+    = Nat_String [] : concat (prefix_positions (map positions xs) 1)
+    where prefix_positions [] _
               = []
-          prefix_positions' (x:xs) n
-              = (map (prefix_position n) x):(prefix_positions' xs (n + 1))
+          prefix_positions (x:xs) n
+              = (map (prefix_position n) x):(prefix_positions xs (n + 1))
+positions (Variable _)
+    = [Nat_String []]
 
-positions :: Signature a => (Infinite_Terms a b) -> [Nat_Strings]
-positions (Function x y) = Nat_String [] : prefix_positions (map positions y)
-positions (Variable _)   = [Nat_String []]
+longest_prefix :: (Signature s, Variables v)
+                  => (Infinite_Terms s v) -> Nat_Strings -> Nat_Strings
+longest_prefix (Function _ []) _
+    = Nat_String []
+longest_prefix (Function _ _) (Nat_String [])
+    = Nat_String []
+longest_prefix (Function f xs) (Nat_String (n:ns))
+    = if arity(f) < n
+      then Nat_String []
+      else prefix_position n (longest_prefix (subterm xs n) (Nat_String ns))
+           where subterm (x:xs) 1 = x
+                 subterm (x:xs) n = subterm xs (n - 1)
+longest_prefix (Variable _) _
+    = Nat_String []
 
--- Example signature and example terms
+data Substitutions s v = Substitution [(v, Infinite_Terms s v)]
 
-type Standard_Terms = Infinite_Terms Char Char
+instance (MyShow s, MyShow v, Signature s, Variables v)
+         => Show (Substitutions s v) where
+    show (Substitution xs) = "{" ++ show' xs True ++ "}"
+        where show' [] _
+                  = ""
+              show' ((x, t):ss) True
+                  = "(" ++ myshow x ++ ", " ++ show t ++ ")" ++ show' ss False
+              show' ((x, t):ss) False
+                  = ", (" ++ myshow x ++ ", " ++ show t ++ ")" ++ show' ss False
+
+substitute_variable :: Variables v
+                       => Substitutions s v -> v -> Infinite_Terms s v
+substitute_variable (Substitution []) x
+    = Variable x
+substitute_variable (Substitution ((y,t):ss)) x
+    = if x == y then t else substitute_variable (Substitution ss) x
+
+substitute :: (Signature s, Variables v)
+              => Substitutions s v -> Infinite_Terms s v -> Infinite_Terms s v
+substitute sigma (Function f xs)
+    = Function f (map (substitute sigma) xs)
+substitute sigma (Variable x)
+    = substitute_variable sigma x
+
+-- Examples
+
+type Standard_Terms         = Infinite_Terms Char Char
+type Standard_Substitutions = Substitutions Char Char
 
 instance Signature Char where
     arity 'a' = 0
@@ -53,6 +99,8 @@ instance Signature Char where
     arity 'h' = 2
     arity _   = error "Character not in signature"
 
+instance Variables Char
+
 f_omega :: Standard_Terms
 f_omega = Function 'f' [f_omega]
 
@@ -61,3 +109,13 @@ h_omega = Function 'h' [h_omega, h_omega]
 
 h_a_f_b :: Standard_Terms
 h_a_f_b = Function 'h' [constant 'a', Function 'f' [constant 'b']]
+
+h_x_omega :: Standard_Terms
+h_x_omega = Function 'h' [Variable 'x', h_x_omega]
+
+h_x_f_y :: Standard_Terms
+h_x_f_y = Function 'h' [Variable 'x', Function 'f' [Variable 'y']]
+
+sigma_1 :: Standard_Substitutions
+sigma_1 = Substitution [('x', Function 'f' [constant 'a']), ('y', constant 'a'),
+                        ('z', constant 'b')]
