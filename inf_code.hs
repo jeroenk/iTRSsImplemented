2d2ea99d9cd21f98ef5fdb8553e67995ba4577d3
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2010-06-13 23:07:35 +0200

    Right steps of Strip Lemma (modulus still to do)

--- inf_code.hs
+++ inf_code.hs
@@ -52,6 +52,12 @@ instance Show NatStrings where
 prefix_position :: Int -> NatStrings -> NatStrings
 prefix_position n (NatString ns) = NatString (n:ns)
 
+is_prefix :: NatStrings -> NatStrings -> Bool
+is_prefix (NatString ns) (NatString ms) = is_prefix' ns ms
+    where is_prefix' [] _          = True
+          is_prefix' (_:_) []      = False
+          is_prefix' (n:ns) (m:ms) = if n == m then is_prefix' ns ms else False
+
 positions :: (Signature s, Variables v)
              => Terms s v -> [NatStrings]
 positions (Function _ xs)
@@ -233,34 +239,34 @@ final_term (ComputablyReduction (Reduction ss _) phi)
 
 descendants_of_position :: (Signature s, Variables v)
                            => NatStrings -> Steps s v -> [NatStrings]
-descendants_of_position (NatString ps) (NatString qs, (Rule l r))
-    = map (\xs -> NatString xs) (descendants' (not (ps >= qs)))
-    where descendants' True
+descendants_of_position ps (qs, (Rule l r))
+    = map (\xs -> NatString xs) (descendants' ps qs (is_prefix qs ps))
+    where descendants' (NatString ps) _ False
               = [ps]
-          descendants' False
+          descendants' (NatString ps) (NatString qs) True
               = map (\xs -> qs ++ xs) (compute_new (drop (length qs) ps))
-          compute_new ps' = compute_new' ps' (get_variable l ps')
+          compute_new ps = compute_new' ps (get_variable l ps)
               where get_variable (Function _ _) []
                         = Nothing
                     get_variable (Function _ xs) (p:ps)
                         = get_variable (xs!!(pred p)) ps
                     get_variable (Variable x) _
                         = Just x
-          compute_new' ps' Nothing  = []
-          compute_new' ps' (Just x) = new_positions r x (get_position l ps') []
+          compute_new' ps Nothing  = []
+          compute_new' ps (Just x) = new_positions r x (get_position l ps) []
               where get_position (Function _ xs) (p:ps)
                         = get_position (xs!!(pred p)) ps
                     get_position (Variable _) ps
                         = ps
-          new_positions (Variable x) y ps' qs'
-              = if x == y then [qs' ++ ps'] else []
-          new_positions (Function _ xs) y ps' qs'
-              = concat (new_positions' xs y ps' qs' 1)
+          new_positions (Variable x) y ps qs
+              = if x == y then [qs ++ ps] else []
+          new_positions (Function _ xs) y ps qs
+              = concat (new_positions' xs y ps qs 1)
           new_positions' [] _ _ _ _
               = []
-          new_positions' (x:xs) y ps' qs' n
-              = (new_positions x y ps' (qs' ++ [n]))
-                :(new_positions' xs y ps' qs' (succ n))
+          new_positions' (x:xs) y ps qs n
+              = (new_positions x y ps (qs ++ [n]))
+                :(new_positions' xs y ps qs (succ n))
 
 descendants_across_step :: (Signature s, Variables v)
                            => [NatStrings] -> Steps s v -> [NatStrings]
@@ -283,21 +289,21 @@ sequence_steps (p:ps) r = (p, r):(sequence_steps ps r)
 
 bottom_steps :: (Signature s, Variables v, RewriteSystem s v r)
                 => ComputablyReductions s v r -> Steps s v -> [Steps s v]
-bottom_steps (ComputablyReduction rs _) (q, r) = bottom_steps' rs q r
-    where bottom_steps' (Reduction _ ps) q s
-              = steps ps [q] r
-          steps [] _ _
+bottom_steps (ComputablyReduction rs _) (q, r) = bottom_steps' rs q
+    where bottom_steps' (Reduction _ ps) q
+              = steps ps [q]
+          steps [] _
               = []
-          steps ((p, r'):ps) qs r
+          steps ((p, r'):ps) qs
               = let down_steps     = sequence_steps qs r
                     descendants_p  = descendants [p] down_steps
                     bottom_steps   = sequence_steps descendants_p r'
                     descendants_qs = descendants qs [(p, r')]
-                in bottom_steps ++ (steps ps descendants_qs r)
+                in bottom_steps ++ (steps ps descendants_qs)
 
 needed_height :: (Signature s, Variables v)
                => RewriteRules s v -> Int
-needed_height (Rule l r) = max 0 (term_height l - term_height r)
+needed_height (Rule l _) = term_height l
     where term_height (Function _ xs) = 1 + foldl max 0 (map term_height xs)
           term_height (Variable _)    = 0
 
@@ -324,6 +330,42 @@ bottom_reduction r s = ComputablyReduction reduction modulus
           steps = bottom_steps r s
           modulus = bottom_modulus r s
 
+right_steps :: (Signature s, Variables v, RewriteSystem s v r)
+               => ComputablyReductions s v r -> Steps s v -> [Steps s v]
+right_steps (ComputablyReduction rs phi) (q, r) = right_steps' rs q r
+    where right_steps' (Reduction _ ps) q r
+              = steps ps [q] 0 0
+          steps _ [] _ _
+              = []
+          steps ps qs m d
+              = right_steps ++ (steps ps_left descendants_nd m_new (succ d))
+              where m_new          = max m (phi d)
+                    ps_use         = take (m_new - m) ps
+                    ps_left        = drop (m_new - m) ps
+                    descendants_qs = descendants qs ps_use
+                    descendants_d  = filter at_d descendants_qs
+                        where at_d (NatString qs) = (length qs) == d
+                    descendants_nd = filter not_at_d descendants_qs
+                        where not_at_d (NatString qs) = (length qs) /= d
+                    right_steps = sequence_steps descendants_d r
+
+right_modulus :: (Signature s, Variables v, RewriteSystem s v r)
+                 => ComputablyReductions s v r -> Steps s v -> Modulus
+right_modulus _ _ = \n -> error "Not implemented"
+
+right_reduction :: (Signature s, Variables v, RewriteSystem s v r)
+                   => ComputablyReductions s v r -> Steps s v
+                   -> ComputablyReductions s v r
+right_reduction r s = ComputablyReduction reduction modulus
+    where reduction = Reduction terms steps
+          terms = (rewrite_steps (final_term r) steps)
+          steps = right_steps r s
+          modulus = right_modulus r s
+
+strip_lemma :: (Signature s, Variables v, RewriteSystem s v r)
+               => r -> ComputablyReductions s v r -> Steps s v
+               -> (ComputablyReductions s v r, ComputablyReductions s v r)
+strip_lemma _ r s = (bottom_reduction r s, right_reduction r s)
 
 -- Examples
 
@@ -368,6 +410,9 @@ h_x_omega = Function 'h' [Variable 'x', h_x_omega]
 h_x_f_y :: Standard_Terms
 h_x_f_y = Function 'h' [Variable 'x', Function 'f' [Variable 'y']]
 
+h_x_f_x :: Standard_Terms
+h_x_f_x = Function 'h' [Variable 'x', Function 'f' [Variable 'x']]
+
 h_x_h_a_x :: Standard_Terms
 h_x_h_a_x = Function 'h' [Variable 'x',
                           Function 'h' [constant 'a', Variable 'x']]
@@ -398,6 +443,11 @@ rule_7 = Rule f_x h_x_x
 
 rule_8 = Rule f_x (constant 'a')
 
+rule_9 = Rule f_x h_x_f_x
+
+rule_10 :: Standard_Rules
+rule_10 = Rule (constant 'a') (constant 'b')
+
 data System_1 = Sys1
 
 instance RewriteSystem Char Char System_1 where
@@ -411,7 +461,7 @@ instance RewriteSystem Char Char System_2 where
 data System_3 = Sys3
 
 instance RewriteSystem Char Char System_3 where
-    rules Sys3 = [rule_5, rule_6, rule_7, rule_8]
+    rules Sys3 = [rule_5, rule_6, rule_7, rule_8, rule_9, rule_10]
 
 red_1 :: (Signature Char, Variables Char, RewriteSystem Char Char System_3)
         => Reductions Char Char System_3
@@ -434,8 +484,19 @@ red_3 = Reduction ts (zip ps rs)
           rs = [rule_4, rule_6]
           ts = rewrite_steps (Function 'f' [h_a_f_b]) (zip ps rs)
 
+red_4 :: (Signature Char, Variables Char, RewriteSystem Char Char System_3)
+        => Reductions Char Char System_3
+red_4 = Reduction ts (zip ps rs)
+    where ps = [NatString [], NatString [2], NatString [2,2]]
+          rs = [rule_9, rule_9, rule_9]
+          ts = rewrite_steps (Function 'f' [constant 'a']) (zip ps rs)
+
 cred_1 = ComputablyReduction red_1 (\x -> succ x)
 
 cred_2 = ComputablyReduction red_2 (\x -> succ x)
 
 cred_3 = ComputablyReduction red_3 (\x -> 2)
+
+cred_4 = ComputablyReduction red_4 (\x -> min 3 (succ x))
+
+show_steps (ComputablyReduction (Reduction _ s) _) = s
