3f7b82cfe183e10e09f3a2c425bce793d6a9bcee
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2010-07-08 23:28:03 +0200

    Notation for omega.2 + 1

--- com_code.hs
+++ com_code.hs
@@ -1,3 +1,5 @@
+-- Systems of notation
+
 data OrdinalType
     = ZeroOrdinal
     | SuccessorOrdinal
@@ -17,3 +19,31 @@ get_limit_pred n = get_limit_pred' (k n) n
 class SystemOfNotation s => UnivalentSystem s where
     leq :: s -> s -> Bool
 
+-- Examples
+
+type OmegaTwoPlusOne = Int
+
+instance SystemOfNotation OmegaTwoPlusOne where
+    k n
+       | n == 0    = LimitOrdinal      -- omega.2
+       | n == 1    = LimitOrdinal      -- omega
+       | n == 2    = ZeroOrdinal       -- 0
+       | otherwise = SuccessorOrdinal  -- even: n; odd: omega + n
+    p n
+       | n > 2     = n - 2
+       | otherwise = error("Predeccessor undefined")
+    q n
+       | n == 0    = (\m -> (2 * m) + 3)
+       | n == 1    = (\m -> (2 * m) + 2)
+       | otherwise = error("Limit function undefined")
+
+instance UnivalentSystem OmegaTwoPlusOne where
+    leq m n
+        | n == m                                   = True
+        | n == 0                                   = True
+        | n == 1 && m > 0              && (even m) = True
+        |           m == 2                         = True
+        | n > 2  && m > 2  && (odd n)  && (odd m)  = m <= n
+        | n > 2  && m > 2  && (odd n)  && (even m) = True
+        | n > 2  && m > 2  && (even n) && (even m) = m <= n
+        | otherwise                                = False
