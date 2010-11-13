7ce60c1fc542d534d3546e61b66dbce423b3e557
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2010-11-13 12:07:42 +0100

    Introduce dynamic signatures, variables, and systems

--- RulesAndSystems.hs
+++ RulesAndSystems.hs
@@ -23,7 +23,8 @@ module RulesAndSystems (
     Step,
     rewrite_step, rewrite_steps,
     descendants, origins_across,
-    RewriteSystem(rules)
+    RewriteSystem(rules),
+    DynamicSystem
 ) where
 
 import MyShow
@@ -116,3 +117,11 @@ origins_across ps s
 -- A rewrite system is a singleton set with an associated rule function.
 class (Signature s, Variables v) => RewriteSystem s v r where
     rules :: r -> [RewriteRule s v]
+
+-- A "dynamic" rewrite system: the rules are simply not know. This still makes
+-- sense in the context of the confluence and compression algorithms, as those
+-- use the rules embedded in the reductions they are applied to.
+data DynamicSystem = DynamicSystem
+
+instance RewriteSystem DynamicSigma DynamicVar DynamicSystem where
+    rules DynamicSystem = error "Rules cannot be queried in dynamic systems"
