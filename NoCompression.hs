96c2535d7461bbe85bf6de93a704fef794cedd83
Jeroen Ketema <j.ketema@ewi.utwente.nl>
2011-05-05 15:45:42 +0200

    Construction of steps and terms along the reduction

--- NoCompression.hs
+++ NoCompression.hs
@@ -16,20 +16,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
 import SignatureAndVariables
-import Substitution
 import Term
 import RuleAndSystem
 import SystemOfNotation
 
 import Array
 
+import Omega
+
 data Sigma = SigmaCons String
 data Var   = VarCons Char
-
-type Symbol_Sigma_Var       = Symbol Sigma Var
-type Term_Sigma_Var         = Term Sigma Var
-type Substitution_Sigma_Var = Substitution Sigma Var
-type Rule_Sigma_Var         = RewriteRule Sigma Var
 
 instance Signature Sigma where
     arity (SigmaCons "c")  = 0
@@ -41,18 +37,18 @@ instance Signature Sigma where
     arity  _ = error "Character not in signature"
 
 instance Eq Sigma where
-    (SigmaCons f) == (SigmaCons g) = f == g
+    (SigmaCons symbol_0) == (SigmaCons symbol_1) = symbol_0 == symbol_1
 
 instance Show Sigma where
-    show (SigmaCons f) = f
+    show (SigmaCons symbol) = symbol
 
 instance Variables Var
 
 instance Eq Var where
-    (VarCons x) == (VarCons y) = x == y
+    (VarCons variable_0) == (VarCons variable_1) = variable_0 == variable_1
 
 instance Show Var where
-    show (VarCons x) = [x]
+    show (VarCons variable) = [variable]
 
 x :: Var
 x = VarCons 'x'
@@ -81,54 +77,54 @@ h' = SigmaCons "h'"
 k :: Sigma
 k = SigmaCons "k"
 
-f_x_x_y :: Term_Sigma_Var
+f_x_x_y :: Term Sigma Var
 f_x_x_y = function_term f [Variable x, Variable x, Variable y]
 
-h_k_y :: Term_Sigma_Var
+h_k_y :: Term Sigma Var
 h_k_y = function_term h [k_y]
     where k_y = function_term k [Variable y]
 
-k_f'_x_y_z :: Term_Sigma_Var
+k_f'_x_y_z :: Term Sigma Var
 k_f'_x_y_z = function_term k [f'_x_y_z]
     where f'_x_y_z = function_term f' [Variable x, Variable y, Variable z]
 
-f_k_x_y_z :: Term_Sigma_Var
+f_k_x_y_z :: Term Sigma Var
 f_k_x_y_z = function_term f [k_x, Variable y, Variable z]
     where k_x = function_term k [Variable x]
 
-k_h'_x :: Term_Sigma_Var
+k_h'_x :: Term Sigma Var
 k_h'_x = function_term k [h'_x]
     where h'_x = function_term h' [Variable x]
 
-h_k_x :: Term_Sigma_Var
+h_k_x :: Term Sigma Var
 h_k_x = function_term h [k_x]
     where k_x = function_term k [Variable x]
 
-k_h_x :: Term_Sigma_Var
+k_h_x :: Term Sigma Var
 k_h_x = function_term k [h_x]
 
-h_x :: Term_Sigma_Var
+h_x :: Term Sigma Var
 h_x = function_term h [Variable x]
 
-h_omega :: Term_Sigma_Var
+h_omega :: Term Sigma Var
 h_omega = function_term h [h_omega]
 
-rule_f :: Rule_Sigma_Var
+rule_f :: RewriteRule Sigma Var
 rule_f = Rule f_x_x_y h_k_y
 
-rule_k_f' :: Rule_Sigma_Var
+rule_k_f' :: RewriteRule Sigma Var
 rule_k_f' = Rule k_f'_x_y_z f_k_x_y_z
 
-rule_k_h' :: Rule_Sigma_Var
+rule_k_h' :: RewriteRule Sigma Var
 rule_k_h' = Rule k_h'_x h_k_x
 
-rule_k_h :: Rule_Sigma_Var
+rule_k_h :: RewriteRule Sigma Var
 rule_k_h = Rule k_h_x h_x
 
-term :: UnivalentSystem o
+construct_term :: UnivalentSystem o
     => (Integer -> Bool) -> (Integer -> Bool) -> (Integer -> o) -> o -> o
-       -> Term_Sigma_Var
-term in_set geq_lub nu alpha beta = replace_c (term' 0 alpha beta)
+       -> Term Sigma Var
+construct_term in_set geq_lub nu alpha beta = replace_c (term' 0 alpha beta)
     where term' d delta gamma
               | in_set d && in_range
                   = function_term f [t_1, t_2, t_3]
@@ -136,30 +132,79 @@ term in_set geq_lub nu alpha beta = replace_c (term' 0 alpha beta)
                   = constant c
               | otherwise
                   = function_term h [term' (d + 1) delta gamma]
-                  where kappa       = nu d
-                        succ_kappa  = ord_succ kappa
-                        in_range    = delta `ord_leq` kappa
-                                          && succ_kappa `ord_leq` gamma
-                        empty_range = (ord_succ delta) `ord_leq` gamma
-                        t_1         = term' (d + 1) delta kappa
-                        t_2         = constant c
-                        t_3         = rename (term' (d + 1) (succ_kappa) gamma)
+                      where kappa       = nu d
+                            succ_kappa  = ord_succ kappa
+                            in_range    = delta `ord_leq` kappa
+                                              && kappa `ord_less` gamma
+                            empty_range = not (delta `ord_less` gamma)
+                            t_1 = term' (d + 1) delta kappa
+                            t_2 = constant c
+                            t_3 = rename (term' (d + 1) (succ_kappa) gamma)
 
-rename :: Term_Sigma_Var -> Term_Sigma_Var
-rename (Function g ts)
-    | g == f    = Function f' (ts // [(1, rename (ts!1))])
-    | g == h    = Function h' (ts // [(1, rename (ts!1))])
-    | g == c    = constant c
-    | otherwise = error "Illegal symbol in constructed term"
+rename :: Term Sigma Var -> Term Sigma Var
+rename (Function symbol ts)
+    | symbol == f = Function f' (ts // [(1, rename (ts!1))])
+    | symbol == h = Function h' (ts // [(1, rename (ts!1))])
+    | symbol == c = constant c
+    | otherwise   = error "Illegal symbol in constructed term"
 rename _
     = error "Illegal symbol in constructed term"
 
-replace_c :: Term_Sigma_Var -> Term_Sigma_Var
-replace_c (Function g ts)
-    | g == c    = h_omega
-    | otherwise = Function g (fmap replace_c ts)
+replace_c :: Term Sigma Var -> Term Sigma Var
+replace_c (Function symbol ts)
+    | symbol == c = h_omega
+    | otherwise   = Function symbol (fmap replace_c ts)
 replace_c (Variable v)
     = (Variable v)
+
+construct_terms_and_steps :: UnivalentSystem o
+    => (Integer -> Bool) -> (Integer -> Bool) -> (Integer -> o) -> o
+       -> o -> (Term Sigma Var, Step Sigma Var)
+construct_terms_and_steps in_set geq_lub nu alpha beta
+    = construct initial_term beta' beta' True
+        where initial_term = construct_term in_set geq_lub nu beta' alpha
+              beta'        = ord_lim_pred beta
+              construct t delta gamma False
+                  | delta `ord_eq` beta = (t, k_step)
+                  | otherwise           = construct t' delta' gamma f_next
+                      where (f_next, k_step) = find_k_step t
+                            t'     = rewrite_step t k_step
+                            delta' = ord_succ delta
+              construct t delta gamma True
+                  | delta `ord_eq` beta = (t, f_step)
+                  | otherwise           = construct t' delta' gamma' False
+                      where f_step = find_f_step in_set nu alpha gamma
+                            t'     = rewrite_step t f_step
+                            delta' = ord_succ delta
+                            gamma' = ord_succ gamma
+
+find_k_step :: Term Sigma Var -> (Bool, Step Sigma Var)
+find_k_step term = (f_next, (position, rule))
+    where (f_next, position, rule) = find_k_step' term
+          find_k_step' (Function symbol ts)
+              | symbol == k = establish_rule (ts!1)
+              | otherwise   = (f_n, 1 : p, r)
+                  where (f_n, p, r) = find_k_step' (ts!1)
+          find_k_step' (Variable _)
+              = error "Illegal symbol in derived term"
+          establish_rule (Function symbol _)
+              | symbol == f' = (False, [], rule_k_f')
+              | symbol == h' = (False, [], rule_k_h')
+              | symbol == h  = (True, [], rule_k_h)
+              | otherwise    = error "Illegal symbol in derived term"
+          establish_rule (Variable _)
+              = error "Illegal symbol in derived term"
+
+find_f_step :: UnivalentSystem o
+    => (Integer -> Bool) -> (Integer -> o) -> o -> o -> Step Sigma Var
+find_f_step in_set nu alpha beta = (find_f_step' 0, rule_f)
+    where find_f_step' d
+              | beta `ord_eq` alpha
+                  = error "No step with requested index"
+              | in_set d && nu(d) `ord_eq` beta
+                  = []
+              | otherwise
+                  = 1 : find_f_step' (d + 1)
 
 -- Reductie stap:
 -- * geen limiet ordinaal: zoek vorige en doe unieke stap
@@ -174,3 +219,21 @@ replace_c (Variable v)
 --    k-stappen, maar deze gebeuren altijd onder de nu^{-1}(d) stap die we
 --    zoeken.
 
+fin_in_set :: Integer -> Bool
+fin_in_set n = n `elem` [1, 3, 5, 7]
+
+fin_geq_lub :: Integer -> Bool
+fin_geq_lub n = n >= 10
+
+fin_nu :: Integer -> Omega
+fin_nu 1 = OmegaElement 2
+fin_nu 3 = OmegaElement 1
+fin_nu 5 = OmegaElement 0
+fin_nu 7 = OmegaElement 3
+fin_nu _ = error "Impossible"
+
+fin_term :: Term Sigma Var
+fin_term = construct_term fin_in_set fin_geq_lub fin_nu (OmegaElement 0) (OmegaElement 4)
+
+fin_ts :: Omega -> (Term Sigma Var, Step Sigma Var)
+fin_ts = construct_terms_and_steps fin_in_set fin_geq_lub fin_nu (OmegaElement 4)
