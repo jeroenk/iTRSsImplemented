64e51ec6dc3b9b07e6c98de64fefff243b46cd6a
Jeroen Ketema <j.ketema@imperial.ac.uk>
2012-12-09 22:14:20 +0000

    Example showing behaviour of standardisation methods
    A better example is needed in which recursion plays
    a role.

--- StandardisationExamples.hs
+++ StandardisationExamples.hs
@@ -34,8 +34,8 @@ import Prelude
 --     compression system_a_a_f_x_g_b cRed
 --     standardisation system_a_a_f_x_g_b cRed
 --
-red :: Omega2Reduction Sigma Var System_a_a_f_x_g_b
-red = RCons ts ss
+red1 :: Omega2Reduction Sigma Var System_a_a_f_x_g_b
+red1 = RCons ts ss
     where ts = constructSequence terms []
           ss = constructSequence steps []
           terms = [a, f_a, f_b, g_b, c]
@@ -43,7 +43,21 @@ red = RCons ts ss
           ps = [[], [1], [], []]
           rs = [rule_a_to_f_a, rule_a_to_b, rule_f_x_to_g_x, rule_g_b_to_c]
 
-cRed :: CReduction Sigma Var System_a_a_f_x_g_b
-cRed = CRCons red phi
-    where phi (OmegaElement 0) m = OmegaElement 4
+cRed1 :: CReduction Sigma Var System_a_a_f_x_g_b
+cRed1 = CRCons red1 phi
+    where phi (OmegaElement 0) _ = OmegaElement 4
+          phi _ _                = error "Illegal modulus"
+
+red2 :: Omega2Reduction Sigma Var System_a_a_f_x_g_b
+red2 = RCons ts ss
+    where ts = constructSequence terms []
+          ss = constructSequence steps []
+          terms = [h_a_a, h_a_f_a, h_a_f_b, h_b_f_b]
+          steps = zip ps rs
+          ps = [[2], [2, 1], [1]]
+          rs = [rule_a_to_f_a, rule_a_to_b, rule_a_to_b]
+
+cRed2 :: CReduction Sigma Var System_a_a_f_x_g_b
+cRed2 = CRCons red2 phi
+    where phi (OmegaElement 0) _ = OmegaElement 3
           phi _ _                = error "Illegal modulus"
