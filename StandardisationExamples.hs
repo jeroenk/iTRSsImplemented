94d811a8cb219ac8ff2e06d03767c2658d1ce358
Jeroen Ketema <j.ketema@imperial.ac.uk>
2012-12-13 15:21:29 +0000

    Standardisation examples in proper module
    This module also exports the compression examples.

--- StandardisationExamples.hs
+++ StandardisationExamples.hs
@@ -15,8 +15,15 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
--- This file defines some reductions that can be tried with the standardisation
--- algorithm.
+-- This module defines some reductions that can be tried with the
+-- standardisation algorithms.
+
+module StandardisationExamples (
+    cRed1, cRed2, cRed3,
+    cRed4, cRed5, cRed6,
+    cRed7, cRed8,
+    compression, standardisation
+) where
 
 import RuleAndSystem
 import Reduction
@@ -25,6 +32,7 @@ import Compression
 import Standardisation
 import ExampleTermsAndSubstitutions
 import ExampleRulesAndSystems
+import CompressionExamples
 
 import Prelude
 
@@ -34,8 +42,8 @@ import Prelude
 --     compression system_a_a_f_x_g_b cRed
 --     standardisation system_a_a_f_x_g_b cRed
 --
-red1 :: Omega2Reduction Sigma Var System_a_f_x_g_b_h_b_a
-red1 = RCons ts ss
+red6 :: Omega2Reduction Sigma Var System_a_f_x_g_b_h_b_a
+red6 = RCons ts ss
     where ts = constructSequence terms []
           ss = constructSequence steps []
           terms = rewriteSteps a steps
@@ -43,13 +51,13 @@ red1 = RCons ts ss
           ps = [[], [1], [], []]
           rs = [rule_a_to_f_a, rule_a_to_b, rule_f_x_to_g_x, rule_g_b_to_c]
 
-cRed1 :: CReduction Sigma Var System_a_f_x_g_b_h_b_a
-cRed1 = CRCons red1 phi
+cRed6 :: CReduction Sigma Var System_a_f_x_g_b_h_b_a
+cRed6 = CRCons red6 phi
     where phi (OmegaElement 0) _ = OmegaElement 4
           phi _ _                = error "Illegal modulus"
 
-red2 :: Omega2Reduction Sigma Var System_a_f_x_g_b_h_b_a
-red2 = RCons ts ss
+red7 :: Omega2Reduction Sigma Var System_a_f_x_g_b_h_b_a
+red7 = RCons ts ss
     where ts = constructSequence terms []
           ss = constructSequence steps []
           terms = rewriteSteps h_a_a steps
@@ -57,16 +65,16 @@ red2 = RCons ts ss
           ps = [[2], [2, 1], [1]]
           rs = [rule_a_to_f_a, rule_a_to_b, rule_a_to_b]
 
-cRed2 :: CReduction Sigma Var System_a_f_x_g_b_h_b_a
-cRed2 = CRCons red2 phi
+cRed7 :: CReduction Sigma Var System_a_f_x_g_b_h_b_a
+cRed7 = CRCons red7 phi
     where phi (OmegaElement 0) _ = OmegaElement 3
           phi _ _                = error "Illegal modulus"
 
 -- Demonstrates for parallel strategy that we works backwards to the needed
 -- steps that were found.
 -- Demonstrates for depth-left strategy that this works recursively.
-red3 :: Omega2Reduction Sigma Var System_a_f_x_g_b_h_b_a
-red3 = RCons ts ss
+red8 :: Omega2Reduction Sigma Var System_a_f_x_g_b_h_b_a
+red8 = RCons ts ss
     where ts = constructSequence terms []
           ss = constructSequence steps []
           terms = rewriteSteps (h h_a_a h_a_a) steps
@@ -74,7 +82,7 @@ red3 = RCons ts ss
           ps = [[2, 1], [1, 2], [1, 2, 1], [1, 1], [1, 2], [1], [1], [2], []]
           rs = [rule_a_to_b, rule_a_to_f_a, rule_a_to_b, rule_a_to_b, rule_f_x_to_a, rule_h_b_a_to_a, rule_a_to_b, rule_h_b_a_to_a, rule_h_b_a_to_a]
 
-cRed3 :: CReduction Sigma Var System_a_f_x_g_b_h_b_a
-cRed3 = CRCons red3 phi
+cRed8 :: CReduction Sigma Var System_a_f_x_g_b_h_b_a
+cRed8 = CRCons red8 phi
     where phi (OmegaElement 0) _ = OmegaElement 9
           phi _ _                = error "Illegal modulus"
